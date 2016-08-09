952dd8160cd6c53b06edb0dfae143dcd36da77a7
Giulianno Sbrugnera <giulianno.sbrugnera@gmail.com>
2016-08-09 14:57:07 -0300

    Controllers added for Author and Book. Scaffolding used to create CRUD Razor views

--- src/MvcLibrary/Data/ApplicationDbContext.cs
+++ src/MvcLibrary/Data/ApplicationDbContext.cs
@@ -22,5 +22,9 @@ namespace MvcLibrary.Data
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
         }
+
+        public DbSet<Author> Author { get; set; }
+
+        public DbSet<Book> Book { get; set; }
     }
 }
