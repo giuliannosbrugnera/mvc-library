6f51b44855e6f62e9230c590ede15a6c77264c11
Giulianno Sbrugnera <giulianno.sbrugnera@gmail.com>
2016-08-12 09:11:37 -0300

    Changed to "5" the default number of items showed inside the tables

--- src/MvcLibrary/wwwroot/js/site.js
+++ src/MvcLibrary/wwwroot/js/site.js
@@ -5,8 +5,7 @@
     // Call the DataTables plugin.
     $("table").dataTable({
         // Disable sorting on last column, where the buttons reside
-        columnDefs: [
-            { orderable: false, targets: -1 }
-        ]
+        "columnDefs": [{ orderable: false, targets: -1 }],
+        "lengthMenu": [[5, 10, 25, 50, 100, -1], [5, 10, 25, 50, 100, "All"]]
     });
 });
